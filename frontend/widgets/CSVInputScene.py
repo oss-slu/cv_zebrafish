55e8869aff8a9f1eb78fae1b1ab05014965ba5cc
jaywin2099 <jaywin2099@gmail.com>
2025-10-06 11:43:41 -0500

    added csv file validation

--- frontend/widgets/CSVInputScene.py
+++ frontend/widgets/CSVInputScene.py
@@ -2,6 +2,21 @@ from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton, QFileDial
 from PyQt5.QtCore import pyqtSignal, Qt, QSize
 from PyQt5.QtGui import QIcon
 
+# imports the input_verifier module from the parent directory 
+from os import getcwd, path
+module_name = "input_verifier"
+parent_dir = path.abspath(path.join(getcwd(), path.pardir))
+file_path = path.join(parent_dir, "data_schema_validation", "src", module_name + ".py")
+
+import importlib.util
+from sys import modules
+
+# loads the csv verification module
+spec = importlib.util.spec_from_file_location(module_name, file_path)
+input_verifier = importlib.util.module_from_spec(spec)
+modules[module_name] = input_verifier
+spec.loader.exec_module(input_verifier)
+
 class CSVInputScene(QWidget):
     csv_selected = pyqtSignal(str)  # emits file path when selected
 
@@ -35,6 +50,23 @@ class CSVInputScene(QWidget):
 
         if file_path:
             self.path_field.setText(file_path)
-            self.csv_selected.emit(file_path)
+            print(f"Validating selected file: {file_path}...")
+
+            # checks for valid csv file format
+            if (self.validate_csv(file_path)):
+                self.csv_selected.emit(file_path)
+            else:
+                print("\nInvalid CSV file format (check schema validation readme)")
         else:
             print("File path error")
+
+    def validate_csv(self, file_path):
+        errors, warnings = input_verifier.verify_deeplabcut_csv(file_path)
+
+        if not errors and not warnings:
+            print("✅ File passed all checks.")
+        else:
+            print("\n".join(errors + warnings))
+
+        return not errors  # returns True if no errors
+
