c78866447b8262e90d5ca7ddca35d1a2f6b67ff8
jaywin2099 <jaywin2099@gmail.com>
2025-10-15 11:52:43 -0500

    added testing toggle

--- frontend/widgets/CalculationScene.py
+++ frontend/widgets/CalculationScene.py
@@ -15,6 +15,7 @@ class CalculationScene(QWidget):
 
         self.csv_path = None
         self.config = None
+        self.previous_settings = { "csv_path": None, "config": None }
 
         layout = QVBoxLayout()
 
@@ -33,15 +34,39 @@ class CalculationScene(QWidget):
         self.calc_button.clicked.connect(self.run_calculation)
         layout.addWidget(self.calc_button)
 
+        # adds toggle to switch between using default config or test config
+        layout.addWidget(QLabel("Toggle to use test config"), alignment=Qt.AlignCenter)
+        self.toggle_button = QPushButton()
+        self.toggle_button.setCheckable(True)
+        self.toggle_button.setIconSize(QSize(40, 40))
+        self.toggle_button.setToolTip("Use Default Config")
+        self.toggle_button.clicked.connect(self.toggle_test)
+        layout.addWidget(self.toggle_button, alignment=Qt.AlignCenter)
+
         self.setLayout(layout)
+    
+    def toggle_test(self):
+        if self.toggle_button.isChecked():
+            self.toggle_button.setToolTip("Using Test Config")
+            self.set_config("/../data_schema_validation/sample_inputs/jsons/BaseConfig.json", update_info=False)
+            self.set_csv_path("/../data_schema_validation/sample_inputs/csv/correct_format.csv")
+        else:
+            self.toggle_button.setToolTip("Use Default Config")
 
-    def set_csv_path(self, path):
+            self.set_config(self.previous_settings["config"])
+            self.set_csv_path(self.previous_settings["csv_path"])
+
+    def set_csv_path(self, path, update_info=True):
+        self.previous_settings["csv_path"] = self.csv_path
         self.csv_path = path
-        self.update_info()
+        if update_info:
+            self.update_info()
 
-    def set_config(self, config):
+    def set_config(self, config, update_info=True):
+        self.previous_settings["config"] = self.config
         self.config = config
-        self.update_info()
+        if update_info:
+            self.update_info()
 
     def update_info(self):
         if self.csv_path and self.config:
