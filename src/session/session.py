6dd22aadd682dc334ac1de591cf86be4272d8d42
jaywin2099 <jaywin2099@gmail.com>
2025-11-25 12:41:19 -0600

    added file validation to stop session from loading files that dont exist anymore

--- src/session/session.py
+++ src/session/session.py
@@ -2,6 +2,7 @@ from os import path
 import json
 
 from PyQt5.QtCore import pyqtSignal, QObject
+from PyQt5.QtWidgets import QMessageBox
 
 from src.app_platform.paths import sessions_dir
 
@@ -14,15 +15,21 @@ class Session(QObject):
         self.csvs = {}
     
     def addCSV(self, csv_path):
+        if not path.exists(csv_path):
+            print(f"[Session] CSV path does not exist: {csv_path}")
+            return
+        
         self.csvs[csv_path] = {}
-
         self.session_updated.emit()
-
+        
     def addConfigToCSV(self, csv_path, config_path):
+        if not path.exists(config_path):
+            print(f"[Session] Config path does not exist: {config_path}")
+            return
+        
         if csv_path in self.csvs:
             self.csvs[csv_path][config_path] = []
-        
-        self.session_updated.emit()
+            self.session_updated.emit()
 
     def addGraphToConfig(self, config_path, graph_path):
         for _, configs in self.csvs.items():
@@ -84,6 +91,30 @@ class Session(QObject):
         if config_path:
             return config_path in self.csvs[csv_path]
         return True
+    
+    '''
+    def removeInvalidEntries(self):
+        removedFiles = []
+
+        for csv_path, configs in self.csvs.items():
+            if not path.exists(csv_path):
+                print(f"[Session] Removing missing CSV: {csv_path}")
+                removedFiles.append(csv_path)
+                del self.csvs[csv_path]
+
+                continue
+            
+            for config_path in list(configs.keys()):
+                if not path.exists(config_path):
+                    print(f"[Session] Removing missing Config: {config_path} from CSV: {csv_path}")
+                    removedFiles.append(config_path)
+                    del configs[config_path]
+        
+        if len(removedFiles) > 0:
+            print(f"[Session] Removed missing files: {removedFiles}")
+            self.session_updated.emit()
+            QMessageBox.information(self.parent(), "Session Update", f"Found missing files in session:\n" + "\n".join(removedFiles) + "\nThey have been removed from the session.")
+    '''
 
 def load_session_from_json(json_path):
     """Load a session from a JSON file."""
